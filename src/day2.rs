d5ec663e8df3826bd7273a521ed8655de2c5c3c9
melkir <melkir13@gmail.com>
2024-12-08 22:35:58 +0900

    feat: day2 part2

--- src/day2.rs
+++ src/day2.rs
@@ -5,14 +5,16 @@ fn parse(input: &str) -> String {
     input.to_string()
 }
 
-fn is_valid(line: &str) -> bool {
-    // For each line we have multiple levels splitted by a whitespace
-    // The levels are either all increasing or all decreasing.
-    // Any two adjacent levels differ by at least one and at most three.
-    let levels = line
-        .split_whitespace()
-        .map(|s| str::parse::<u32>(s).unwrap())
-        .collect::<Vec<_>>();
+fn parse_levels(line: &str) -> Vec<u32> {
+    line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn is_safe(levels: &[u32]) -> bool {
+    if levels.len() < 2 {
+        return true;
+    }
     let inc = levels.windows(2).all(|p| p[0] < p[1]);
     let dec = levels.windows(2).all(|p| p[0] > p[1]);
     let diff_adj = levels.windows(2).all(|p| p[0].abs_diff(p[1]) <= 3);
@@ -21,19 +23,40 @@ fn is_valid(line: &str) -> bool {
 
 #[aoc(day2, part1)]
 fn part1(input: &str) -> u32 {
-    let mut count = 0;
-    for line in input.lines() {
-        let val = is_valid(&line);
-        if val == true {
-            count += 1;
-        }
-    }
-    count
+    input
+        .lines()
+        .filter(|line| {
+            let levels = parse_levels(line);
+            is_safe(&levels)
+        })
+        .count() as u32
 }
 
 #[aoc(day2, part2)]
 fn part2(input: &str) -> u32 {
-    todo!()
+    input
+        .lines()
+        .filter(|line| is_safe_with_dampener(line))
+        .count() as u32
+}
+
+fn is_safe_with_dampener(report: &str) -> bool {
+    let levels: Vec<u32> = parse_levels(report);
+
+    if is_safe(&levels) {
+        return true;
+    }
+
+    // Check if the Problem Dampener can make the report safe
+    for i in 0..levels.len() {
+        let mut dampened_levels = levels.clone();
+        dampened_levels.remove(i);
+        if is_safe(&dampened_levels) {
+            return true;
+        }
+    }
+
+    false
 }
 
 #[cfg(test)]
@@ -55,6 +78,7 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), 0);
+        let input = std::fs::read_to_string("input/2024/day2.txt").expect("Failed to read file");
+        assert_eq!(part2(&parse(&input)), 612);
     }
 }
